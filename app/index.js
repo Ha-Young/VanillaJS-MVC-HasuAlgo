54e840fd54ee5bf0c28be8a37d1f67f8ff6439b0
onemo <wonmotime1@naver.com>
2021-02-01 18:01:27 +0900

    add font size, weight etc

--- app/index.js
+++ app/index.js
@@ -1,9 +1,4 @@
 // Load application styles
 import '../assets/styles/index.less';
 
-// ================================
-// START YOUR APP HERE
-// ================================
-
 console.log('여기서 작업하세요!');
-  
