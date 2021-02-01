6b67c28eefe877b4352de9e5e061187bc3bb758a
onemo <wonmotime1@naver.com>
2021-02-01 19:07:22 +0900

    changed css and class name

--- app/index.js
+++ app/index.js
@@ -1,4 +1,9 @@
 // Load application styles
 import '../assets/styles/index.less';
 
-console.log('여기서 작업하세요!');
+const $typed = document.querySelector(".typed");
+
+document.querySelector('.inputText').addEventListener('submit', function () {
+  event.preventDefault();
+  console.log($typed.value);
+});
