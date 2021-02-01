98621fce34cb075e7a2016631fb2b7f5c43c8c26
Ha0 <hayeong28@gmail.com>
2021-02-01 23:44:25 +0900

    add: sort-kinds-btns click event console.log

--- app/view.js
+++ app/view.js
@@ -1,7 +1,22 @@
+import {$on, qs, $delegate} from './helpers';
+
 export default class View {
-	/**
-	 * @param {!Template} template A Template instance
-	 */
-	constructor(template) {
+	constructor() {
+		console.log('View Constructor!');
+
+		this.$themeSwitch = qs('.theme-switch');
+		this.$sortKindsBtns = qs('.sort-kinds-btns');
+		this.$inputNumbers = qs('.input-numbers');
+		this.$startBtn = qs('.start-btn');
+		this.$visualList = qs('.visual-list');
+
+
+	}
+
+	bindOnClickSortKindsBtns(handler) {
+		$on(this.$sortKindsBtns, 'click', ({currentTarget, target}) => {
+			console.log('currentTarget', currentTarget);
+			console.log('target', target);
+		});
 	}
 }
