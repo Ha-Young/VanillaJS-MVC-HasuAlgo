88a51b47fb7110c2bfe475c49e7a461ddb03b8d6
JUNGJuhyoung <fheh1992@gmail.com>
2021-02-01 16:04:04 +0900

    Work on CSS, Html

--- app/index.js
+++ app/index.js
@@ -1,8 +1,44 @@
 // Load application styles
 import '../assets/styles/index.less';
 
-// ================================
-// START YOUR APP HERE
-// ================================
+const $userInputBtn = document.querySelector('.user-input-btn');
+const $userInputTable = document.querySelector('.user-input-table');
+const $graphTable = document.querySelector('.article');
 
-console.log('여기서 작업하세요!');
+console.log($userInputBtn);
+$userInputBtn.addEventListener('click', ev => {
+  console.log($userInputTable.value);
+});
+
+class Model {
+  constructor() {
+    this.userInputData = [];
+  }
+
+  set(value) {
+    return this.userInputData.push(value);
+  }
+
+  get(value) {
+    return this.userInputData.indexOf(value);
+  }
+}
+
+class View {
+  createGraph(graphValue) {
+    $graphTable.innerHTML = `<div class="graph-item"></div>`;
+  }
+}
+
+class Controller {
+  constructor(model, view) {
+    this.model = model;
+    this.view = view;
+  }
+}
+
+const app = new Controller(new Model(), new View());
+
+const modelTest = new Model();
+console.log(modelTest);
+modelTest.set('4');
