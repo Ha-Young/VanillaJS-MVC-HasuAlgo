fb4bae97121218fa4df2bf92610776fb1b126813
ha-young <hayeong28@naver.com>
2021-02-01 20:58:54 +0900

    add: Model.js

--- app/model.js
+++ app/model.js
@@ -1,8 +1,27 @@
+const LS_THEME = 'theme';
+
 export default class Model {
-	/**
-	 * @param {!string} name Database name
-	 */
-	constructor(name) {
-		
-	}
+	constructor() {
+    const localStorage = window.localStorage;
+		const sortList = [];
+  }
+
+  initDatas(data) {
+    if (!data && typeof data !== 'string') {
+      throw new Error("Model's initDatas data parameter is not right");
+    }
+
+    const splitData = data.split(',').trim();
+    splitData.map((char) => {
+      const parsedInt = parseInt(char);
+
+      if (parsedInt === NaN) {
+        // Error 처리
+      }
+
+      return parsedInt;
+    });
+
+    this.sortList = splitData;
+  }
 }
