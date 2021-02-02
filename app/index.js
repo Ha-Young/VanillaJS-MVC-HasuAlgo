94fdd22f26f7c41de5a8cef63cf946fc1a179bde
eluka22 <eluka22@gmail.com>
2021-02-02 11:38:46 +0900

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,8 +1,28 @@
 // Load application styles
 import '../assets/styles/index.less';
+import '../assets/styles/normalize.less';
 
 // ================================
 // START YOUR APP HERE
 // ================================
 
-console.log('여기서 작업하세요!');
+import { View } from './view';
+import { Model } from './model.js';
+import { Controller } from './controller';
+
+function Graph() {
+  this.data = new Model();
+  this.view = new View();
+  this.controller = new Controller(this.data, this.view);
+}
+
+const graph = new Graph();
+
+graph.controller.clickEvent(graph.view.inputButton, function() {
+  const resultCheckData = graph.controller.checkData();
+  const resultCheckSort = graph.controller.checkSort();
+
+  if (resultCheckData && resultCheckSort) {
+    graph.view.renderGraph(graph.data.dataArray);
+  }
+})
