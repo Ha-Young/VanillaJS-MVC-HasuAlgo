06366c96b383bfddbea3313f472c08b1fdae062c
eluka22 <eluka22@gmail.com>
2021-02-02 11:39:13 +0900

    Update view.js

--- app/view.js
+++ app/view.js
@@ -1,26 +1,55 @@
-export { checkData };
+export { View };
 
-function checkData(data) {
-  const $validationText = document.querySelector(".validation-text");
-  const filteredData = data.replaceAll(',', '').replace(/ /g, '');
-  const dataArray = data.replace(/ /g, '').split(",");
+class View {
 
-  if (isNaN(filteredData)) {
-    $validationText.classList.remove("hidden");
-    $validationText.textContent = "Only number";
-    console.log("aaa");
-    return;
+  constructor() {
+    this.validation = document.querySelector(".validation-text");
+
+    this.selectbox = document.getElementById("sort-selection");
+    this.options = this.selectbox.getElementsByTagName("option");
+
+    this.graphList = document.querySelector(".graph-list");
+    this.graphs = document.querySelectorAll(".graph-list li");
+
+    this.inputButton = document.querySelector(".input-button");
+    this.input= document.querySelector(".input-value");
+
+    this.validationText = {
+      isNaN: "Only Number",
+      over10: "Only under 10 elements",
+      under5: "Over 5 elements",
+      over50: "only under 50 numbers",
+      sort: "Select sort",
+    }
   }
 
-  if (dataArray.length > 10) {
-    $validationText.textContent = "Only under 10 elements";
-    return;
+  createElement(tag, className) {
+    const element = document.createElement(tag)
+    if (className) element.classList.add(className)
+
+    return element;
   }
 
-  for (let i = 0; i < dataArray.length; i++) {
-    if (dataArray[i] > 50) {
-      $validationText.textContent = "Only under 50 number";
-      break;
+  changeValidation(text) {
+    this.validation.classList.remove("hidden");
+    this.validation.textContent = text;
+  }
+
+  renderGraph(array) {
+    for (let i = 0; i < this.graphs.length; i++) {
+      this.graphs[i].remove();
+    }
+
+    for (let i = 0; i < array.length; i++) {
+      const graph = document.createElement("li");
+      const number = document.createElement("span");
+      const max = Math.max(...array);
+
+      graph.setAttribute("data-value", array[i]);
+      graph.style.height = 250 * (array[i] / max) + "px";
+      number.textContent = array[i];
+      graph.appendChild(number);
+      this.graphList.appendChild(graph);
     }
   }
 }
