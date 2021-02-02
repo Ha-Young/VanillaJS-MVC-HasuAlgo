95aacfa1ca03eb872a9564987278e54e035bad2a
eluka22 <eluka22@gmail.com>
2021-02-03 00:04:05 +0900

    Update bubble sort

--- app/controller.js
+++ app/controller.js
@@ -19,7 +19,7 @@ class Controller {
   checkData() {
     this.model.data = this.getInputValue();
     this.model.filteredData = this.model.data.replaceAll(',', '').replace(/ /g, '');
-    this.model.dataArray = this.model.data.replace(/ /g, '').split(",");
+    this.model.dataArray = this.model.data.replace(/ /g, '').split(",").map((num) => Number(num));
 
     if (isNaN(this.model.filteredData)) {
       this.view.changeValidation(this.view.validationText["isNaN"]);
@@ -56,14 +56,29 @@ class Controller {
   }
 
   doBubbleSort(dataArray) {
-    for (let i = 0; i < dataArray.length; i++) {
-      for (let j = 0; j < dataArray.length - 1; j++) {
-        if (dataArray[j] > dataArray[j+1]) {
-          const big = dataArray[j];
-          dataArray[j] = dataArray[j+1];
-          dataArray[j+1] = big;
+    (async () => {
+      const graphs = Array.prototype.slice.call(document.querySelectorAll(".graph-list li"));
+      this.view.graphs = graphs;
+      debugger;
+      for (let i = 0; i < dataArray.length; i++) {
+        for (let j = 0; j < dataArray.length - i - 1; j++) {
+          if (dataArray[j] > dataArray[j+1]) {
+            const big = dataArray[j];
+            dataArray[j] = dataArray[j+1];
+            dataArray[j+1] = big;
+            await this.view.selectGraph(this.view.graphs[j], this.view.graphs[j+1], "selected");
+            await this.view.swapGraph(this.view.graphs[j], this.view.graphs[j+1], j, j+1);
+            await this.view.deselectGraph(this.view.graphs[j], this.view.graphs[j+1], "selected");
+            if ((j + 1) === dataArray.length - i - 1) {
+              await this.view.confirmGraph(this.view.graphs[j+1], "confirmed");
+            }
+          } else {
+            await this.view.selectGraph(this.view.graphs[j], this.view.graphs[j+1], "stopped");
+            await this.view.deselectGraph(this.view.graphs[j], this.view.graphs[j+1], "stopped");
+          }
         }
+        await this.view.confirmGraph(this.view.graphs[dataArray.length - i - 1], "confirmed");
       }
-    }
+    })();
   }
 }
