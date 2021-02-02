d8b3d55e1ea15f92dec27c829acaed9b4a384c8e
ha-young <hayeong28@gmail.com>
2021-02-02 14:03:50 +0900

    add: sort items template

---
+++ app/typeDef.js
@@ -0,0 +1,24 @@
+/**
+ * @typedef {!{x: number, y: number}}
+ */
+export var Position;
+
+/**
+ * @typedef {!{width: number, height: number}}
+ */
+export var Retc;
+
+/**
+ * @typedef {!{x: number, y: number, size: string}}
+ */
+export var Text;
+
+/**
+ * @typedef {!{position: Position, rect: Rect, text: Text, value: string}}
+ */
+export var SortItem;
+
+/**
+ * @typedef {!Array<SortItem>}
+ */
+export var SortItemList;
