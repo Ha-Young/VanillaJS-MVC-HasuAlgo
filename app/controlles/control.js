cd09c315151cfa30573ec39be9deacb2d0113597
yunsu <than4502@gmail.com>
2021-02-02 16:36:16 +0900

    Add reset button

--- app/controlles/control.js
+++ app/controlles/control.js
@@ -5,6 +5,7 @@ const $userInput = document.querySelector('.user-value');
 const $visual = document.querySelector('.visual');
 const $start = document.querySelector('.start');
 const $delete = document.querySelector('.delete');
+const $reset = document.querySelector('.reset');
 const test = [];
 
 function makeItem(b) {
@@ -16,19 +17,22 @@ function makeItem(b) {
     $visual.append($box);
     test.push($userInput.value);
     $userInput.value = '';
-    
-    return;
-  }
-  
-  if (b.key === 'Backspace') {
-    $visual.removeChild($visual.lastChild);
-    test.pop();
-    $userInput.value = '';
   }
 }
 
 function deleteItem() {
+  $visual.removeChild($visual.lastChild);
+  test.pop();
+}
 
+function resetItem() {
+  const leng = test.length;
+
+  for (let i = 0; i < leng; i++) {
+    test.pop();
+    $visual.removeChild($visual.lastChild);
+    $userInput.value = '';
+  }
 }
 
 function runSort() {
@@ -44,6 +48,7 @@ function init() {
   $userInput.addEventListener('keyup', makeItem);
   $delete.addEventListener('click', deleteItem);
   $start.addEventListener('click', runSort);
+  $reset.addEventListener('click', resetItem);
 }
 
 init();
