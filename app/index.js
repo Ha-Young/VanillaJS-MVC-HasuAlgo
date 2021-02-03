0478877455cd542ae8e5879132d2c0bfde7fea70
eluka22 <eluka22@gmail.com>
2021-02-03 16:46:54 +0900

    Add checking quicksort option

--- app/index.js
+++ app/index.js
@@ -23,27 +23,34 @@ graph.controller.clickEvent(graph.view.inputButton, function() {
   const resultCheckSort = graph.controller.checkSort();
 
   if (resultCheckData && resultCheckSort) {
-    graph.view.clearGraph();
     graph.view.renderGraph(graph.model.unsortedArray);
     graph.view.removeClass(graph.view.sortButton, "invisible");
   }
 });
 
 graph.controller.clickEvent(graph.view.sortButton, function() {
-  graph.controller.doBubbleSort(graph.model.sortedArray);
+  if (graph.controller.checkSort() === 1) {
+    graph.controller.doBubbleSort(graph.model.sortedArray);
+  } else {
+    graph.controller.doQuickSort(graph.model.sortedArray);
+  }
+
   graph.view.addClass(graph.view.sortButton, "invisible");
-  graph.view.removeClass(graph.view.sortRestartButton, "invisible");
   graph.view.removeClass(graph.view.sortClearButton, "invisible");
 });
 
 graph.controller.clickEvent(graph.view.sortRestartButton, function() {
-  graph.view.clearGraph();
+  if (graph.controller.checkSort() === 1) {
+    graph.controller.doBubbleSort(graph.model.sortedArray);
+  } else {
+    graph.controller.doQuickSort(graph.model.sortedArray);
+  }
+
   graph.view.renderGraph(graph.model.unsortedArray);
   graph.controller.doBubbleSort(graph.model.sortedArray);
 });
 
 graph.controller.clickEvent(graph.view.sortClearButton, function() {
   graph.view.clearGraph();
-  graph.view.addClass(graph.view.sortRestartButton, "invisible");
   graph.view.addClass(graph.view.sortClearButton, "invisible");
 })
