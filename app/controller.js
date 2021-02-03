ac94f5e953cf02b979b913cd357032d24dd7d6f7
eluka22 <eluka22@gmail.com>
2021-02-03 16:13:17 +0900

    Fix doBubbleSort method

--- app/controller.js
+++ app/controller.js
@@ -17,25 +17,26 @@ class Controller {
   checkData() {
     this.model.data = this.getInputValue();
     this.model.filteredData = this.model.data.replaceAll(',', '').replace(/ /g, '');
-    this.model.dataArray = this.model.data.replace(/ /g, '').split(",").map((num) => Number(num));
+    this.model.unsortedArray = this.model.data.replace(/ /g, '').split(",").map((num) => Number(num));
+    this.model.sortedArray = this.model.unsortedArray.slice();
 
     if (isNaN(this.model.filteredData)) {
       this.view.changeValidation(this.view.validationText["isNaN"]);
       return false;
     }
 
-    if (this.model.dataArray.length > 10) {
+    if (this.model.unsortedArray.length > 10) {
       this.view.changeValidation(this.view.validationText["over10"]);
       return false;
     }
 
-    if (this.model.dataArray.length < 5) {
+    if (this.model.unsortedArray.length < 5) {
       this.view.changeValidation(this.view.validationText["under5"]);
       return false;
     }
 
-    for (let i = 0; i < this.model.dataArray.length; i++) {
-      if (this.model.dataArray[i] > 50) {
+    for (let i = 0; i < this.model.unsortedArray.length; i++) {
+      if (this.model.unsortedArray[i] > 50) {
         this.view.changeValidation(this.view.validationText["over50"]);
         return false;
       }
@@ -53,31 +54,31 @@ class Controller {
     }
   }
 
-  doBubbleSort(dataArray) {
+  async doBubbleSort(dataArray) {
     const graphs = this.view.graphs = Array.prototype.slice.call(document.querySelectorAll(".graph-list li"));
-    (async () => {
-      for (let i = 0; i < dataArray.length; i++) {
-        for (let j = 0; j < dataArray.length - i - 1; j++) {
-          if (dataArray[j] > dataArray[j+1]) {
-            const bigNumber = dataArray[j];
+    console.log(dataArray);
+    for (let i = 0; i < dataArray.length; i++) {
+      for (let j = 0; j < dataArray.length - i - 1; j++) {
+        if (dataArray[j] > dataArray[j+1]) {
+          const bigNumber = dataArray[j];
 
-            dataArray[j] = dataArray[j+1];
-            dataArray[j+1] = bigNumber;
+          dataArray[j] = dataArray[j+1];
+          dataArray[j+1] = bigNumber;
 
-            await this.view.selectGraph(graphs[j], graphs[j+1], "selected");
-            await this.view.swapGraph(graphs[j], graphs[j+1], j, j+1);
-            await this.view.deselectGraph(graphs[j], graphs[j+1], "selected");
+          await this.view.selectGraph(graphs[j], graphs[j+1], "selected");
+          await this.view.swapGraph(graphs[j], graphs[j+1], j, j+1);
+          await this.view.deselectGraph(graphs[j], graphs[j+1], "selected");
 
-            if ((j + 1) === dataArray.length - i - 1) {
-              await this.view.confirmGraph(graphs[j+1], "confirmed");
-            }
-          } else {
-            await this.view.selectGraph(graphs[j], graphs[j+1], "stopped");
-            await this.view.deselectGraph(graphs[j], graphs[j+1], "stopped");
+          if ((j + 1) === dataArray.length - i - 1) {
+            await this.view.confirmGraph(graphs[j+1], "confirmed");
           }
+        } else {
+          await this.view.selectGraph(graphs[j], graphs[j+1], "stopped");
+          await this.view.deselectGraph(graphs[j], graphs[j+1], "stopped");
         }
-        await this.view.confirmGraph(graphs[dataArray.length - i - 1], "confirmed");
       }
-    })();
+      await this.view.confirmGraph(graphs[dataArray.length - i - 1], "confirmed");
+    }
+    this.model.sortedArray = this.model.unsortedArray.slice();
   }
 }
