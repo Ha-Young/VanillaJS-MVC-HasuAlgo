80e8808d1c1c5e6431b7e694998a46749eb5702b
yunsu <than4502@gmail.com>
2021-02-03 22:44:36 +0900

    Add bind this in event function

--- app/controlles/control.js
+++ app/controlles/control.js
@@ -1,45 +1,43 @@
 import { BubbleModel } from '../models/model';
 import { BubbleView } from '../views/view';
 
-const $userInput = document.querySelector('.user-value');
-const $start = document.querySelector('.start');
-const $delete = document.querySelector('.delete');
-const $reset = document.querySelector('.reset');
-const bubbleView = new BubbleView();
-const test = [];
-const isFull = false;
-
 export const BubbleController = function() {
-  //this.$userInput = document.querySelector('.user-value');
+  this.$userInput = document.querySelector('.user-value');
+  this.$start = document.querySelector('.start');
+  this.$delete = document.querySelector('.delete');
+  this.$reset = document.querySelector('.reset');
+  this.bubbleView = new BubbleView();
+  this.test = [];
 }
 
-
 BubbleController.prototype.addItem = function(e) {
-  let value = $userInput.value;
-  //console.log(this.$userInput, $userInput);
+  let value = this.$userInput.value;
+
   if (e.key === 'Enter' && value !== '') {
-    if (test.length > 9) {
+    if (this.test.length > 9) {
       alert("10개 이상의 수는 비교 할 수 없습니다..")
-      $userInput.value = '';
+
+      this.$userInput.value = '';
 
       return;
     }
 
-    if (!filterType(value)) {
+    if (!this.filterType(value)) {
       alert('숫자만 입력 가능합니다!');
-      $userInput.value = '';
+      this.$userInput.value = '';
 
       return;
     }
 
-    test.push(value);
-    $userInput.value = '';
+    this.test.push(value);
+
+    this.$userInput.value = '';
     
-    bubbleView.addItem(value);
+    this.bubbleView.addItem(value);
   }
 }
 
-function filterType(input) {
+BubbleController.prototype.filterType = function(input) {
   let filtering = [...input];
   let isNumber = true;
   
@@ -53,40 +51,40 @@ function filterType(input) {
 }
 
 BubbleController.prototype.deleteItem = function() {
-  if (!test.length) {
-    $userInput.value = '';
+  if (!this.test.length) {
+    this.$userInput.value = '';
     return;
   }
   
-  test.pop();
-  bubbleView.deleteItem();
+  this.test.pop();
+  this.bubbleView.deleteItem();
 }
 
 BubbleController.prototype.resetItem = function() {
-  const leng = test.length;
+  const leng = this.test.length;
   
   for (let i = 0; i < leng; i++) {
-    test.pop();
+    this.test.pop();
   }
 
-  $userInput.value = '';
-  bubbleView.resetItem(leng);
+  this.$userInput.value = '';
+  this.bubbleView.resetItem(leng);
 }
 
 BubbleController.prototype.startSort = function() {
-  if (!test.length) {
-    $userInput.value = '';
+  if (!this.test.length) {
+    this.$userInput.value = '';
 
     return;
   }
 
-  if (test.length < 5) {
-    alert("정렬할 수를 5개 이상 만들어주세요! (10개 넘으면 안되유..)");
+  if (this.test.length < 5) {
+    alert("정렬할 수를 5개 이상 만들어주세요! (10개는 넘으면 안돼요;;)");
 
     return;
   }
 
-  const refineValue = test.map(n => {
+  const refineValue = this.test.map(n => {
     return Number(n);
   });
   
@@ -95,16 +93,16 @@ BubbleController.prototype.startSort = function() {
 }
 
 BubbleController.prototype.startSwap = async function(a, b) {
-  await bubbleView.swap(a, b);
+  await this.bubbleView.swap(a, b);
 }
 
 BubbleController.prototype.paintBox = function(a, b, n) {
-  bubbleView.paint(a, b, n);
+  this.bubbleView.paint(a, b, n);
 }
 
 BubbleController.prototype.init = function() {
-  $userInput.addEventListener('keyup', this.addItem);
-  $delete.addEventListener('click', this.deleteItem);
-  $start.addEventListener('click', this.startSort);
-  $reset.addEventListener('click', this.resetItem);
+  this.$userInput.addEventListener('keyup', this.addItem.bind(this));
+  this.$delete.addEventListener('click', this.deleteItem.bind(this));
+  this.$start.addEventListener('click', this.startSort.bind(this));
+  this.$reset.addEventListener('click', this.resetItem.bind(this));
 }
