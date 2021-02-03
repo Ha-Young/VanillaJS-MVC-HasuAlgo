ced4a520c9200618e7fd7857dae095b53c3d32be
kwon, minho <minhob38@gmail.com>
2021-02-03 20:44:17 +0900

    Update bubble sort and animation

--- app/controller.js
+++ app/controller.js
@@ -5,25 +5,25 @@ export default function Controller(model, view) {
 
 // controller event 등록 in View, this = controller
 Controller.prototype.addEvent = function () {
-  this.view.addControllerEvent('.run-button', 'click', this.runAnimation.bind(this)); // selector, handle function
+  this.view.addControllerEvent('.play-button', 'click', this.runAnimation.bind(this)); // selector, handle function
 }
 
 
 // controller event handler function
 // run 버튼을 누르면, 데이터 가져오고 읽고 수정하고 애니메이션 그림
 Controller.prototype.runAnimation = function () { // this = controller (bind)
-  const inputData = this.getDatafromView('.input-data', 'value'); //view에서 입력 데이터 가져오기
+  const inputData = this.getDatafromView('.input-data-box', 'value'); //view에서 입력 데이터 가져오기
   const manipulatedData = this.manipulateDataFromView(inputData); //view에서 받은 데이터 검증 및 가공
   this.sendDataToModel(manipulatedData);  
   console.log(this.model.storage)                        // 가공한 입력데이터를 Model Storage에 넣음
   this.initializeView(this.model.storage);
-  this.updateView();
+  this.updateView(this.model.storage);
 }
 
 //Model 관련 메소드
 //view를 통해 받은 데이터를 가공 검증
 Controller.prototype.manipulateDataFromView = function (data) {
-  const validRe = /\[[\d\s]+\]/g;
+  const validRe = /[\d\s]+/g;
 
   if (!validRe.test(data)) {
     alert('[숫자 숫자 숫자] 형식으로 입력해주세요!');
@@ -52,78 +52,36 @@ Controller.prototype.initializeView = function (data) {
 }
 
 //controller를 이용해, view를 렌더링하자..
-Controller.prototype.updateView = async function () {
-  this.sort()
+Controller.prototype.updateView = async function (data) {
+  this.sort(data)
 }
 
-//<----3S------>view <---- view3s ---->
-//                   <---- CSS 3s----->
-
-Controller.prototype.sort = async function () {
-  const data = this.model.storage;
+Controller.prototype.sort = async function (data) {
+  const delayTime = 2000;
+  let sortCount = 0;
   let j = 0;
-  
+
   while (j < data.length) {
     for (let i = 0; i < data.length; i++) {
+      sortCount++;
+
       if(data[i] > data[i + 1]) {
         const temp = data[i];
         data[i] = data[i + 1];
         data[i + 1] = temp;
       }
-      let delayTime = i === 0 ? 2000 : 2000;
+
+      const sortData = {data, i, sortCount};
       const delay = new Promise((resolve, reject) => {
         setTimeout(() => {
-          resolve(data);
+          resolve(sortData);
         }, delayTime);
       });
-      
+
       delay.then(this.view.render);
       await delay;
     }
-    j++
-  }  
+
+    j++;
+  }
 }
-
-// {
-// console.log(i)
-//   if(data[i] > data[i + 1]) {
-//     const temp = data[i];
-//     data[i] = data[i + 1];
-//     data[i + 1] = temp;
-//   }
-//   let delayTime = i === 0 ? 10000 : 10000;
-//   const delay = new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       console.log('sort')
-//       console.log(data)
-//       resolve(data);
-//     }, delayTime);
-//   });
-  
-//   await delay; // await까지 기다림
-
-//   delay.then(this.view.render); //비동기 코드므로, 아래 동기 코드들이 먼저 실행됨
-
-// }
-
-// {
-// console.log(i)
-//   if(data[i] > data[i + 1]) {
-//     const temp = data[i];
-//     data[i] = data[i + 1];
-//     data[i + 1] = temp;
-//   }
-//   let delayTime = i === 0 ? 10000 : 10000;
-//   const delay = new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       console.log('sort')
-//       console.log(data)
-//       resolve(data);
-//     }, delayTime);
-//   });
-  
-//   await delay;
-
-//   delay.then(this.view.render);
-
-// }
--- app/view.js
+++ app/view.js
@@ -1,21 +1,35 @@
+//import a from "../assets/images/chicken.jpg"
 export default function View() {
   const $template = document.createAttribute('template');
   const $root = document.querySelector('.root');
 
   $template.innerHTML = `
     <section class="container">
-      <header class="title-box">Sort Alogrithm</header>
-      <section class="control-box">
-        <div>
-          <input type="radio" name="sort-type"/><label>Bubble Sorting</label>
-          <input type="radio" name="sort-type"/><label>Merged Sorting</label>
-        </div>
-        <button class="run-button">Run</button>
+      <header class="title-box">
+        Sort Algorithm
+      </header>
+      <section class="user-input-container">
+        <section class="sort-select-box">
+          <div>
+            <input type="radio" name="sort-type"/><label>Bubble Sorting</label>
+          </div>
+          <div>
+            <input type="radio" name="sort-type"/><label>Merged Sorting</label>
+          </div>
+        </section>
+        <input class="input-data-box" type="text" placeholder="Insert Data"/>
+        <section class="control-box">
+          <button class="play-button">Play</button>
+          <button class="pause-button">Pause</button>
+        </section>
       </section>
-      <section class="input-box">
-        <input class="input-data" type="text" placeholder="Insert Data by the following format ---> [3 5 16 2 11 32]""/>
-      </section>
-      <main class="animation-box"></main>
+      <main class="animation-container">
+        <section  class="status-box">
+          Let's Sort!
+        </section>
+        <section class="animation-box">
+        </section>
+      </main>
     </section>
     `;
 
@@ -23,7 +37,7 @@ export default function View() {
 };
 
 View.prototype.addControllerEvent = function (target, event, handler) {
-  if (target === '.run-button') {
+  if (target === '.play-button') {
     document.querySelector(target).addEventListener(event, handler);
   }
 }
@@ -48,22 +62,38 @@ View.prototype.initialRender = function (data) {
     $animationBox.children[i].style.height = `${200 * data[i] / maxData}px`;
   }
 
-  $animationBox.classList.add('run');
+  $animationBox.classList.add('play');
 }
 
-View.prototype.render = function (data) {
+View.prototype.render = function (sortData) {
   const $animationBox = document.querySelector('.animation-box');
-  const maxData = Math.max(...data);
+  const $statusBox = document.querySelector('.status-box');
+  const maxData = Math.max(...sortData.data);
   const animationBoxHeight = $animationBox.style.height;
-  for (let i = 0; i < data.length; i++) {
-    $animationBox.children[i].textContent = data[i];
-    $animationBox.children[i].style.height = `${200 * data[i] / maxData}px`;
+  let ordinalNumberSuffix;
+
+  switch (sortData.sortCount % 10) {
+    case 1:
+      ordinalNumberSuffix = 'st';
+      break;
+    case 2:
+      ordinalNumberSuffix = 'nd';
+      break;
+    case 3:
+      ordinalNumberSuffix = 'rd';
+      break;
+    default:
+      ordinalNumberSuffix = 'th';
   }
-  
-  //$animationBox.insertBefore($animationBox.children[1], $animationBox.children[0])
 
-  $animationBox.classList.add('run');
+  $animationBox.children[(sortData.i + 4) % sortData.data.length].classList.remove('play');
+  $animationBox.children[sortData.i].classList.add('play');
+  $animationBox.children[sortData.i].textContent = sortData.data[sortData.i];
+  $animationBox.children[sortData.i].style.height = `${200 * sortData.data[sortData.i] / maxData}px`;
+  $animationBox.children[(sortData.i + 1) % sortData.data.length].classList.add('play');
+  $animationBox.children[(sortData.i + 1) % sortData.data.length].textContent = sortData.data[(sortData.i + 1) % sortData.data.length];
+  $animationBox.children[(sortData.i + 1) % sortData.data.length].style.height = `${200 * sortData.data[(sortData.i + 1) % sortData.data.length] / maxData}px`;
+  $statusBox.textContent = `Searching ${sortData.sortCount}${ordinalNumberSuffix}`;
 
-  //insertBefore
   return;
 }
