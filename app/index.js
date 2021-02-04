fd9549514f2bc0d2279d24a1135a03dabb4abfaa
minwoo kim <vvee1253@gmail.com>
2021-02-04 20:57:04 +0900

    Add footer background

--- app/index.js
+++ app/index.js
@@ -1,6 +1,11 @@
 // Load application styles
 import '../assets/styles/reset.less';
 import '../assets/styles/index.less';
+import '../assets/styles/button.less';
+import '../assets/styles/input.scss';
+
+
+// Load mario styles
 import '../assets/styles/background.less';
 import '../assets/styles/mario.less';
 
