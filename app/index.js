8b154dc443177be9929c1cb5ff686335f555fdb9
minwoo kim <vvee1253@gmail.com>
2021-02-04 19:36:17 +0900

    Add images

--- app/index.js
+++ app/index.js
@@ -1,6 +1,8 @@
 // Load application styles
 import '../assets/styles/reset.less';
 import '../assets/styles/index.less';
+import '../assets/styles/background.less';
+import '../assets/styles/mario.less';
 
 // ================================
 // START YOUR APP HERE
