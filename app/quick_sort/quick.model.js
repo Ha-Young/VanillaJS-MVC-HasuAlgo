8f19872291d9dbd6729ddcbf561d684cfed5ba9b
a01081440011 <a01081440011@gamil.com>
2021-02-05 19:52:44 +0900

    reomove useless things...

--- app/quick_sort/quick.model.js
+++ app/quick_sort/quick.model.js
@@ -6,22 +6,3 @@ export default function QuickModel() {
 
 QuickModel.prototype = Object.create(Model.prototype);
 QuickModel.prototype.constructor = QuickModel;
-
-// QuickModel.prototype.get = function () {
-//   return this._storage;
-// };
-
-// QuickModel.prototype.set = function (dataSet) {
-//   this._storage = dataSet;
-
-//   return this;
-// };
-
-// QuickModel.prototype.swap = function (left, right) {
-//   const temp = this._storage[left];
-
-//   this._storage[left] = this._storage[right];
-//   this._storage[right] = temp;
-
-//   return this;
-// };
