0ed528ae89174785e5439d77db70a61a3212a5f2
Hansol Yun <hsyoon315@gmail.com>
2021-02-05 18:35:21 +0900

    Refactor createController

--- app/controller.js
+++ app/controller.js
@@ -2,16 +2,18 @@ import {view} from "./view.js";
 import {model} from "./model.js";
 
 function createController() {
-  const startButton = document.querySelector(".start-button");
-  const userInput = document.querySelector(".user-input");
+  const $startButton = document.querySelector(".start-button");
+  const $userInput = document.querySelector(".user-input");
+  const $bubbleSortButton = document.querySelector(".bubble-sort-button");
+  let isBubbleSortTurn = false;
 
   function convertNumber() {
-    const inputValueList = userInput.value.split(",").map(Number);
+    const inputValueList = $userInput.value.split(",").map( num => Number(num));
 
     for (const item of inputValueList) {
       if (isNaN(item)) {
-        setTimeout(view.showWarningMessage, 300);
-        setTimeout(view.removeWarningMessage, 1000);
+        setTimeout(view.warningMessage.inputNumber, 300);
+        setTimeout(view.warningMessage.removeMessage, 1000);
         return;
       }
     }
@@ -19,66 +21,81 @@ function createController() {
     return inputValueList;
   }
 
+  $userInput.addEventListener("input", convertNumber);
 
-  userInput.addEventListener("input", convertNumber);
+  $startButton.addEventListener("click", function handleInputItem() {
+    $userInput.removeEventListener("input", convertNumber);
 
-  startButton.addEventListener("click", function handleInputItem() {
-    userInput.removeEventListener("input", convertNumber);
     const numberList = convertNumber();
 
-    if (numberList.length > 5 && numberList.length < 10) {
-      controller.addItem(numberList);
-      controller.bubbleSort();
+    if (numberList.length > 4 && numberList.length < 10) {
+      controller.addList(numberList);
+      if (isBubbleSortTurn) {
+        controller.bubbleSort();
+        isBubbleSortTurn = false;
+      }
+
       this.removeEventListener("click", handleInputItem);
     }
 
     view.removeInputValue();
   });
 
+  $bubbleSortButton.addEventListener("click", function () {
+    view.removeContent();
+
+    isBubbleSortTurn = true;
+  });
+
   return {
-    addItem: function (list) {
+    addList: function (list) {
       model.addList(list);
+      model.cacheUserInputList();
       view.showBlock(model.storage);
     },
 
-    bubbleSort: async function () {
-      let blockList = document.querySelectorAll(".block");
+    removeItem: function (list) {
+      model.removeList(list);
+    },
 
-      for (let i = 0; i < blockList.length - 1; i++) {
-        for (let j = 0; j < blockList.length - i - 1; j++) {
-          if (model.sortedStorage.length !== 0) {
-            model.removeList(blockList);
+    bubbleSort: function () {
+      const taskElementList = [];
+      const storage = model.storage[0];
+
+      for (let i = 0; i < storage.length - 1; i++) {
+        for (let j = 0; j < storage.length - i - 1; j++) {
+          taskElementList.push(controller.inputTask("COMPARE", j, j + 1));
+          
+          if (storage[j] > storage[j + 1]) {
+            const temp = storage[j];
+            storage[j] = storage[j + 1];
+            storage[j + 1] = temp;
+            
+            taskElementList.push(controller.inputTask("SWAP", j, j + 1));
           }
 
-          await controller.delay();
-          blockList[j].style.backgroundColor = "#f0e6fc";
-          blockList[j + 1].style.backgroundColor = "#f0e6fc";
-          await controller.delay();
-
-          const prevItem = Number(blockList[j].textContent);
-          const nextItem = Number(blockList[j + 1].textContent);
-  
-          if (prevItem > nextItem) {
-            await view.swap(blockList[j], blockList[j + 1]);
-            blockList = document.querySelectorAll(".block");
-          }
-
-          await controller.delay();
-          blockList[j].style.backgroundColor = "white";
-          blockList[j + 1].style.backgroundColor = "white";
-          await controller.delay();
+          taskElementList.push(controller.inputTask("SWAP_DONE", j, j + 1));
         }
-
-        blockList[blockList.length - i - 1].style.backgroundColor = "#f9fac0";
+    
+        taskElementList.push(controller.inputTask("SINGLE_DONE", i));
       }
+      
+      taskElementList.push(controller.inputTask("FINISH"));
+      view.visualize(taskElementList);
+    },
 
-      blockList[0].style.backgroundColor = "#f9fac0";
+    inputTask: function (type, leftIndex, rightIndex) {
+      return {
+        type,
+        leftIndex,
+        rightIndex
+      };
     },
 
     delay: function () {
-      return new Promise((resolve) => {
+      return new Promise( resolve => {
         setTimeout(() => { resolve() }, 300);
-      })
+      });
     }
   };
 }
