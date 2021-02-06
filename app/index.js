a1c374b60ce886e90601846ce9413620495d6ac7
minwoo kim <vvee1253@gmail.com>
2021-02-06 17:25:35 +0900

    Fix error due to calling not-existing file

--- app/index.js
+++ app/index.js
@@ -1,8 +1,8 @@
 // Load application styles
 import '../assets/styles/index.less';
 import '../assets/styles/reset.less';
-import '../assets/styles/sort.less';
 import '../assets/styles/mario.less';
+import '../assets/styles/sort.less';
 
 import { addEventListeners } from '../view/add-event-listeners';
 
